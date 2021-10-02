3dff62d5e4d7f04f11a704e98d03e274779b6a21
ILhankhondaker <ilhankhondaker@gmail.com>
2021-10-03 02:41:01 +0600

    f

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,12 +7,15 @@ import {
   faInstagramSquare,
   faTwitterSquare,
   faYoutube,
+  faLinkedin,
+  faFacebookF
 } from "@fortawesome/free-brands-svg-icons";
 import {
   faCoffee,
   faPhoneVolume,
   faMapMarkedAlt,
 } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -25,16 +28,34 @@ const Footer = () => {
                 <h1>Biona Sports Club</h1>
                 <div className="icons-container d-flex text-center ">
                   <div className="icon">
-                    <FontAwesomeIcon icon={faInstagramSquare} />
+                    <a target="*" rel="noreferrer" href={"https://www.instagram.com/ilhankhondaker/?hl=en/"} >
+                      <FontAwesomeIcon icon={faInstagramSquare} />
+                    </a>
                   </div>
                   <div className="icon">
-                    <FontAwesomeIcon icon={faTwitterSquare} />
+                    <a target="*" rel="noreferrer" href={"https://twitter.com/IlhanKhondaker/"} >
+                      <FontAwesomeIcon icon={faTwitterSquare} />
+                    </a>
                   </div>
                   <div className="icon">
-                    <FontAwesomeIcon icon={faYoutube} />
+                    <a target="*" rel="noreferrer" href={"https://www.youtube.com/channel/UCYzmpgr_xPjbyU7P_xWz2zw"} >
+                      <FontAwesomeIcon icon={faYoutube} />
+                    </a>
                   </div>
                   <div className="icon">
-                    <FontAwesomeIcon icon={faFacebookSquare} />
+                    <a target="*" rel="noreferrer" href={"https://www.facebook.com/ilhan.khondaker/"} >
+                      <FontAwesomeIcon icon={faFacebookSquare} />
+                    </a>
+                  </div>
+                  <div className="icon">
+                    <a target="*" rel="noreferrer" href={"https://www.facebook.com/Ilhan-Khondaker-102758198228272/"} >
+                      <FontAwesomeIcon icon={faFacebookF} />
+                    </a>
+                  </div>
+                  <div className="icon">
+                    <a target="*" rel="noreferrer" href={"https://www.linkedin.com/in/ilhan-khondaker-9a46b01b5//"} >
+                      <FontAwesomeIcon icon={faLinkedin} />
+                    </a>
                   </div>
                 </div>
                 <p className="mt-4 ">
@@ -46,7 +67,7 @@ const Footer = () => {
                 </p>
 
                 <p className="mt-5">
-                  <small>Zemez © . All rights reserved.</small>
+                  <small>Ilhan © . All rights reserved.</small>
                 </p>
               </div>
             </div>
@@ -74,7 +95,7 @@ const Footer = () => {
                     <FontAwesomeIcon icon={faPhoneVolume} />
                   </div>
                   <div>
-                    <h5>+1 8 800 555 35 35</h5>
+                    <h5>+8801680576091</h5>
                   </div>
                 </div>
                 <div className="map d-flex align-items-center justify-content-center">
@@ -93,7 +114,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
